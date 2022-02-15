d5c96e9bc0540fd13da37273f039906ea022d130
Stephane Perret <misochan1978@hotmail.com>
2022-02-15 20:20:33 +0100

    rails-watch-list added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,25 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// Bootstrap import
+import "bootstrap";
+import 'select2/dist/css/select2.min.css'
+import 'jquery-bar-rating/dist/themes/css-stars';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+import { initTyped } from './plugins/init_typedjs';
+import { initSelect2 } from './plugins/init_select2';
+import { initStarRating } from './plugins/init_star_rating';
+
+
+document.addEventListener('turbolinks:load', () => {
+  initSelect2();
+  initTyped();
+  AOS.init();
+  initStarRating();
+});
