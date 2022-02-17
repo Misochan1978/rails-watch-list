c2311d83a39e4fe87a30f2c5cbbe1310f145d09a
Stephane Perret <misochan1978@hotmail.com>
2022-02-17 21:03:51 +0100

    Set up dotenv

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,7 +17,7 @@ import "bootstrap";
 import 'select2/dist/css/select2.min.css'
 import 'jquery-bar-rating/dist/themes/css-stars';
 import 'aos/dist/aos.css';
-import AOS from 'aos';
+i/* import AOS from 'aos'; */
 import { initTyped } from './plugins/init_typedjs';
 import { initSelect2 } from './plugins/init_select2';
 import { initStarRating } from './plugins/init_star_rating';
@@ -26,6 +26,6 @@ import { initStarRating } from './plugins/init_star_rating';
 document.addEventListener('turbolinks:load', () => {
   initSelect2();
   initTyped();
-  AOS.init();
+  /* AOS.init(); */
   initStarRating();
 });
